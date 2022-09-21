f8feab07c0ed934111d6c5e2c8ddb4d9e2728ddb
IvaFil <filipovichiv@yandex.ru>
2022-09-21 11:47:41 +0300

    Задача 21

--- ConsoleApp8/Program.cs
+++ ConsoleApp8/Program.cs
@@ -10,18 +10,28 @@ namespace ConsoleApp8
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Введи число: ");
+            
+            Console.WriteLine("Введи X1: ");
+            double x1 = Convert.ToDouble(Console.ReadLine());
 
-            string number = Convert.ToString(Console.ReadLine());
+            Console.WriteLine("Введи Y1: ");
+            double y1 = Convert.ToDouble(Console.ReadLine());
 
-            if (number[0] == number[4] && number[1] == number[3])
-            {
-                Console.WriteLine("Да");
-            }
-            else
-            {
-                Console.WriteLine("Нет");
-            }
+            Console.WriteLine("Введи Z1: ");
+            double z1 = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Введи X2: ");
+            double x2 = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Введи Y2: ");
+            double y2 = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Введи Z2: ");
+            double z2 = Convert.ToDouble(Console.ReadLine());
+
+            double res = (x1 - x2) * (x1 - x2) + (y1 - y2) * (y1 - y2) + (z1 - z2) * (z1 - z2);
+
+            Console.WriteLine(Math.Sqrt(res));
         }
     }
 }
