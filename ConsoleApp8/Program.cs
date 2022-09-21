cf3ec666c3b62a27a6d8d7af4d8ef48d82f56d21
IvaFil <filipovichiv@yandex.ru>
2022-09-21 11:51:54 +0300

    Задача 23

--- ConsoleApp8/Program.cs
+++ ConsoleApp8/Program.cs
@@ -10,28 +10,17 @@ namespace ConsoleApp8
     {
         static void Main(string[] args)
         {
+
+            Console.WriteLine("Введи число: ");
+
+            int number = Convert.ToInt32(Console.ReadLine());
+            int res=0;
+            for (int i = 1; i <=number; i++)
+            {
+                res = i * i * i;
+                Console.WriteLine(res);
+            }
             
-            Console.WriteLine("Введи X1: ");
-            double x1 = Convert.ToDouble(Console.ReadLine());
-
-            Console.WriteLine("Введи Y1: ");
-            double y1 = Convert.ToDouble(Console.ReadLine());
-
-            Console.WriteLine("Введи Z1: ");
-            double z1 = Convert.ToDouble(Console.ReadLine());
-
-            Console.WriteLine("Введи X2: ");
-            double x2 = Convert.ToDouble(Console.ReadLine());
-
-            Console.WriteLine("Введи Y2: ");
-            double y2 = Convert.ToDouble(Console.ReadLine());
-
-            Console.WriteLine("Введи Z2: ");
-            double z2 = Convert.ToDouble(Console.ReadLine());
-
-            double res = (x1 - x2) * (x1 - x2) + (y1 - y2) * (y1 - y2) + (z1 - z2) * (z1 - z2);
-
-            Console.WriteLine(Math.Sqrt(res));
         }
     }
 }
